54dd51d3bafd87ebc850932163e798e58669d782
SebnemSK <skuzulugil@gmail.com>
2024-07-05 17:10:26 -0400

    Additional dplyr verbs

--- Tidyverse_examples.R
+++ Tidyverse_examples.R
@@ -15,3 +15,50 @@ mydata %>%
   head(n=7) %>%
   # count :)
   count()
+
+mynewdata <- mydata %>%
+  # order by conc desc
+  arrange(desc(conc))
+
+mydata %>% 
+  arrange(Plant, desc(conc))
+
+mynewdata <- mydata %>%
+  # add a new column that's 10% increased uptake
+  mutate(madeup=uptake*1.1)
+
+# madeup=1.1uptake only for c hilled samples
+mynewdata <- mydata %>%
+  mutate(madeup=uptake) %>%
+  filter(Treatment == "chilled") %>%
+  # add a new column that's 10% increased uptake
+  mutate(madeup=uptake*1.1)
+
+# try again ...
+newdata <- mydata %>%
+  mutate(madeup= case_when(
+    Treatment == "chilled" ~ uptake * 1.1,
+    .default = uptake
+  ))
+
+# Summary statistics
+newdata %>%
+  group_by(Type) %>%
+  summarise(NoOfSamples=n(), StdDev=sd(madeup))
+
+# Summary statistics on two column
+newdata %>%
+  group_by(Type, Treatment) %>%
+  summarise(NoOfSamples=n(), StdDev=sd(madeup))
+
+# Add stddev columns calculated by groups 
+newdata <- newdata %>%
+  # create 'buckets'
+  group_by(Type, Treatment) %>%
+  # calculate stddev for each bucket
+  mutate(StdDev=sd(madeup)) %>%
+  # ungroup so that biuckets are destroyed
+  ungroup()
+
+
+  
